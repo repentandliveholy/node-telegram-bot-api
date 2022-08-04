08f517a5bd8f897d07b3681948516a14b6cb8da8
repentandliveholy <80513172+repentandliveholy@users.noreply.github.com>
2022-08-04 22:49:18 +0300

    new

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ if (majorVersion <= 5) {
 } else {
   module.exports = require('./src/telegram');
 }
+
+/**
+ * If running on Nodejs 5.x and below, we load the transpiled code.
+ * Otherwise, we use the ES6 code.
+ * We are deprecating support for Node.js v5.x and below.
+ */
